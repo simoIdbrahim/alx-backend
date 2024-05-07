111ec1a272fad77d1129d68f8b2e49efa6410b9b
simoIdbrahim <mohamedidbrahim16@gmail.com>
2024-05-07 08:22:27 -0700

    add

---
+++ 0x00-pagination/2-hypermedia_pagination.py
@@ -0,0 +1,66 @@
+#!/usr/bin/env python3
+"""
+contains class with methods
+"""
+import csv
+from typing import List
+index_range = __import__('0-simple_helper_function').index_range
+
+
+class Server:
+    """
+    Server class
+    """
+    FILE = "Popular_Baby_Names.csv"
+
+    def __init__(self):
+        self.__dataset = None
+
+    def dataset(self) -> List[List]:
+        """
+        Reads from csv file and returns the dataset.
+        """
+        if self.__dataset is None:
+            with open(self.FILE) as f:
+                reader = csv.reader(f)
+                dataset = [row for row in reader]
+            self.__dataset = dataset[1:]
+
+        return self.__dataset
+
+    @staticmethod
+    def assert_positive_integer_type(val: int) -> None:
+        """
+        def assert_positive_integer_type
+        """
+        assert type(val) is int and val > 0
+
+    def get_page(self, page: int = 1, page_size: int = 10) -> List[List]:
+        """
+        def get_page
+        """
+        self.assert_positive_integer_type(page)
+        self.assert_positive_integer_type(page_size)
+        dataset = self.dataset()
+        start, end = index_range(page, page_size)
+        try:
+            data = dataset[start:end]
+        except IndexError:
+            data = []
+        return data
+
+    def get_hyper(self, page: int = 1, page_size: int = 10) -> dict:
+        """
+        def get_hyper
+        """
+        total_pages = len(self.dataset()) // page_size + 1
+        data = self.get_page(page, page_size)
+        info = {
+            "page": page,
+            "page_size": page_size if page_size <= len(data) else len(data),
+            "total_pages": total_pages,
+            "data": data,
+            "prev_page": page - 1 if page > 1 else None,
+            "next_page": page + 1 if page + 1 <= total_pages else None
+        }
+        return info

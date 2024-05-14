80a238af48bb803ed8bba305de70cf1ddbb102cc
simoIdbrahim <mohamedidbrahim16@gmail.com>
2024-05-14 07:47:32 -0700

    add

--- 0x02-i18n/3-app.py
+++ 0x02-i18n/3-app.py
@@ -1,30 +1,31 @@
 #!/usr/bin/env python3
-""" flask app"""
-from flask import Flask, request, render_template
+""" flask app """
 from flask_babel import Babel
+from flask import Flask, render_template, request
 
 
-class Config(object):
-    """ Config class """
-    LANGUAGES = ['en', 'fr']
-    BABEL_DEFAULT_LOCALE = 'en'
-    BABEL_DEFAULT_TIMEZONE = 'UTC'
+class Config:
+    """ Config class"""
+    LANGUAGES = ["en", "fr"]
+    BABEL_DEFAULT_LOCALE = "en"
+    BABEL_DEFAULT_TIMEZONE = "UTC"
 
 
 app = Flask(__name__)
 app.config.from_object(Config)
+app.url_map.strict_slashes = False
 babel = Babel(app)
 
 
 @babel.localeselector
 def get_locale() -> str:
-    """ get_locale localeselector """
-    return request.accept_languages.best_match(app.config['LANGUAGES'])
+    """ """
+    return request.accept_languages.best_match(app.config["LANGUAGES"])
 
 
-@app.route('/', strict_slashes=False)
-def index() -> str:
-    """ index page"""
+@app.route('/')
+def get_index() -> str:
+    """ index page """
     return render_template('3-index.html')
 
 
